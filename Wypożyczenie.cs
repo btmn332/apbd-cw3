16e78d8d859066093c386d1aa77cf5a3ea7ed7ee
Maksym <maksym.wyrozebski@gmail.com>
2026-03-22 08:56:29 +0100

    Wypożyczenie

---
+++ Wypożyczenie.cs
@@ -0,0 +1,19 @@
+﻿namespace apbd_cw3;
+
+public class Wypożyczenie
+{
+    public DateOnly DataWypożyczenia { get; set; }
+    public DateOnly TerminZwrotu { get; set; }
+    public DateOnly DataZwrotu { get; set; }
+    public Użytkownik Użytkownik { get; set; }
+    public Sprzęt Sprzęt { get; set; }
+    public int Opłaty { get; set; }
+
+    public Wypożyczenie(DateOnly dataWypożyczenia, DateOnly terminZwrotu, Użytkownik użytkownik, Sprzęt sprzęt)
+    {
+        DataWypożyczenia = dataWypożyczenia;
+        TerminZwrotu = terminZwrotu;
+        Użytkownik = użytkownik;
+        Sprzęt = sprzęt;
+    }
+}
